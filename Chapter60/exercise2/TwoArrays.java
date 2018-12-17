dbbf793aceb28597b31fedc2143abb8b0696dbac
Ritvik Durgempudi <rintu1121@outlook.com>
2018-12-17 07:44:56 -0700

    Work on exercises 60-67

---
+++ Chapter60/exercise2/TwoArrays.java
@@ -0,0 +1,15 @@
+class Exercise2
+{
+  public static void main ( String[] args )
+  {
+    int[] val = {13, -4, 82, 17};
+    int[] twice;
+
+    System.out.println( "Original Array: "
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+ 
+
+    System.out.println( "New Array: "
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+   }
+}
